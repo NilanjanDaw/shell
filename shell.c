097857e7ac047d9216f30228cb0ac3388fd2dade
nilanjandaw <nilanjandaw@gmail.com>
2018-07-19 19:08:07 +0530

    shell function implementation of cd history

--- shell.c
+++ shell.c
@@ -1,3 +1,10 @@
+/*
+ * Filename: /home/nilanjan/Desktop/cs744/lab1/shell.c
+ * Path: /home/nilanjan/Desktop/cs744/lab1
+ * Created Date: Thursday, July 19th 2018, 4:10:33 pm
+ * Author: nilanjandaw
+ */
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -8,8 +15,9 @@
 #define MAX_INPUT_SIZE 1024
 #define MAX_TOKEN_SIZE 64
 #define MAX_NUM_TOKENS 64
-
-const char* SHELL_IMPLEMENTED_COMMANDS[] = {"cd", "history", "rpn", "plan"};
+#define HISTORY_KEEPER_PATH "/tmp/history"
+const char* SHELL_IMPLEMENTED_COMMANDS[] = {"cd", "history"};
+const int (*SHELL_IMPLEMENTED_COMMANDS_POINTER[2]) (const char **);
 
 char **tokenize(char *line) {
   char **tokens = (char **)malloc(MAX_NUM_TOKENS * sizeof(char *));
@@ -40,10 +48,38 @@ char **tokenize(char *line) {
 int check_shell_implementation(char *command) {
   int implementation_count = sizeof(SHELL_IMPLEMENTED_COMMANDS)/sizeof(SHELL_IMPLEMENTED_COMMANDS[0]);
   for(size_t i = 0; i < implementation_count; i++) {
-    printf("%s\n", SHELL_IMPLEMENTED_COMMANDS[i]);
     if (strcmp(SHELL_IMPLEMENTED_COMMANDS[i], command) == 0)
       return i;
   }
+  return -1;
+}
+
+int cd (const char **tokens) {
+  int size = 0;
+  for (int i = 0; tokens[i] != NULL; i++)
+    size++;
+  if (size > 2) {
+    printf("too many arguments\n");
+    return -1;
+  }
+  if (size == 2)
+    chdir(tokens[1]);
+  else
+    chdir(getenv("HOME"));
+  return 0;
+}
+
+int history(const char **tokens) {
+  printf("history\n");
+  int c;
+  FILE *history_keeper;
+  history_keeper = fopen(HISTORY_KEEPER_PATH, "r");
+  if (history_keeper) {
+      while ((c = getc(history_keeper)) != EOF)
+          putchar(c);
+      fclose(history_keeper);
+  }
+
   return 0;
 }
 
@@ -59,9 +95,10 @@ int start_process(char **tokens) {
   if (pid == 0) {
     int return_code = execvp(tokens[0], tokens);
     int implementation_code = check_shell_implementation(tokens[0]);
+
     if (return_code < 0 && implementation_code >= 0) {
-      printf("will need shell implementation");
-      return_code = implementation_code;
+      return_code = SHELL_IMPLEMENTED_COMMANDS_POINTER[implementation_code] ((const char **)tokens);
+      
     }
     if (return_code < 0) {
       printf("error in running command\n");
@@ -71,7 +108,7 @@ int start_process(char **tokens) {
     if (wait_return_code < 0) {
       fprintf(stdin, "error in running command\n");
     }
-    printf("Parent %d child pid %d\n", getpid(), (int) pid);
+    //printf("Parent %d child pid %d\n", getpid(), (int) pid);
   }
   
   return 0;
@@ -83,21 +120,29 @@ int main(int argc, char const *argv[])
   char  line[MAX_INPUT_SIZE];            
   char  **tokens;              
   int i;
-
+  
+  SHELL_IMPLEMENTED_COMMANDS_POINTER[0] = cd;
+  SHELL_IMPLEMENTED_COMMANDS_POINTER[1] = history;
+  
   while (1) {           
+    FILE *history_keeper = fopen(HISTORY_KEEPER_PATH, "a");
     printf("Hello>");     
     bzero(line, MAX_INPUT_SIZE);
-    fgets(line, MAX_INPUT_SIZE, stdin);           
+    fgets(line, MAX_INPUT_SIZE, stdin);  
+    fprintf(history_keeper, "%s", line);
     line[strlen(line)] = '\n'; 
     tokens = tokenize(line);
 
     int status = start_process(tokens);
-    
     // Freeing the allocated memory	
     for(i=0;tokens[i]!=NULL;i++){
         free(tokens[i]);
     }
     free(tokens);
+    fclose(history_keeper);
   }
+
+  
+  printf("closed history");
   return 0;
 }
